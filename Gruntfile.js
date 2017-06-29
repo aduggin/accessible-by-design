333e29e58c169bd1c2dd99812e025722cc740339
Alistair Duggin <alistair.duggin@btinternet.com>
2017-06-29 02:32:13 +0100

    Add aXe

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,18 +2,38 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
 
-        accessibility: { // begin
+        accessibility: { // begin HTML_CodeSniffer
             options : {
                 accessibilityLevel: 'WCAG2A'
             },
             test : {
                 src: ['index.html']
             }
+        },
+
+        "axe-webdriver": { // begin aXe
+            phantomjs: {
+                options: {
+                    browser: 'phantomjs',
+                    tags: ['wcag2a', 'wcag2aa', 'wcag2aaa']
+                },
+                urls: ['index.html'],
+                dest: "axeReport.json"
+            }
         }
 
     });
 
     grunt.loadNpmTasks('grunt-accessibility');
+    grunt.loadNpmTasks('grunt-axe-webdriver');
 
-    grunt.registerTask('default', ['accessibility']);
+    grunt.registerTask('default', ['axe', 'sniffer']);
+
+    grunt.registerTask('axe', [
+        'axe-webdriver'
+    ]);
+
+    grunt.registerTask('sniffer', [
+        'accessibility'
+    ]);
 };
